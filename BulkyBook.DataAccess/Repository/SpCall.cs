693feb2aa3ae9271bebdfbf506340a48c8970282
Hien Nguyen <hien_d_nguyen@outlook.com>
2020-05-15 22:03:42 -0600

    Add unit of work to repository

---
+++ BulkyBook.DataAccess/Repository/SpCall.cs
@@ -0,0 +1,88 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class SpCall : ISpCall
+    {
+        private readonly ApplicationDbContext _db;
+        private static string _connectionString = "";
+
+        public SpCall(ApplicationDbContext db)
+        {
+            _db = db;
+            _connectionString = db.Database.GetDbConnection().ConnectionString;
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Execute(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(_connectionString))
+            {
+                sqlcon.Open();
+                sqlcon.Execute(procedureName, param, commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+
+        public IEnumerable<T> List<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(_connectionString))
+            {
+                sqlcon.Open();
+                return sqlcon.Query<T>(procedureName, param, commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+
+        public Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(_connectionString))
+            {
+                sqlcon.Open();
+                var result = SqlMapper.QueryMultiple(sqlcon, procedureName, param, commandType: System.Data.CommandType.StoredProcedure);
+                var item1 = result.Read<T1>().ToList();
+                var item2 = result.Read<T2>().ToList();
+
+                if (item1 !=null && item2 != null)
+                {
+                    return new Tuple<IEnumerable<T1>, IEnumerable<T2>>(item1, item2);
+                }
+            }
+
+            return new Tuple<IEnumerable<T1>, IEnumerable<T2>>(new List<T1>(), new List<T2>());
+        }
+
+        public T OneRecord<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(_connectionString))
+            {
+                sqlcon.Open();
+                var value =  sqlcon.Query<T>(procedureName, param, commandType: System.Data.CommandType.StoredProcedure);
+                return (T)Convert.ChangeType(value.FirstOrDefault<T>(), typeof(T));
+            }
+        }
+
+        public T Single<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(_connectionString))
+            {
+                sqlcon.Open();
+                var value = sqlcon.ExecuteScalar<T>(procedureName, param, commandType: System.Data.CommandType.StoredProcedure);
+                return (T)Convert.ChangeType(value, typeof(T));
+            }
+        }
+    }
+}
