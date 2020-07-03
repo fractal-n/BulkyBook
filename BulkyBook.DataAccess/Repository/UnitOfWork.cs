d97f870516c1c51a13f3fec74eb3a8b44efa9b01
Hien Nguyen <hien_d_nguyen@outlook.com>
2020-07-03 15:27:48 -0600

    Add Company view and controller

--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -16,12 +16,14 @@ namespace BulkyBook.DataAccess.Repository
             Category = new CategoryRepository(_db);
             CoverType = new CoverTypeRepository(_db);
             Product = new ProductRepository(_db);
+            Company = new CompanyRepository(_db);
             SpCall = new SpCall(_db);
         }
 
         public ICategoryRepository Category { get; private set; }
         public ICoverTypeRepository CoverType { get; private set; }
         public IProductRepository Product { get; private set; }
+        public ICompanyRepository Company { get; private set; }
 
         public ISpCall SpCall { get; private set; }
 
